ed6185553f41d63a602927604041cf2b0464a1e0
mechanicus01 <mechanicus01@gmail.com>
2022-11-18 21:31:32 -0600

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,5 @@
+# If you do not have a Powershell profile, run this command to create one. The default direcory is the Documents folder. 
+# New-Item $PROFILE.CurrentUserAllHosts -ItemType File -Force
+
 # Function to access tool.exe from anywhere on the system 
 function iwtool.exe { C:\'Program Files\IceWarp\tool.exe' $args }
