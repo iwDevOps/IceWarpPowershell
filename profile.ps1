b661079576d10c90b4872e52c83b33d7c99459f3
mechanicus01 <mechanicus01@gmail.com>
2023-01-08 18:47:31 -0600

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,12 +1,12 @@
 # If you do not have a Powershell profile, run this command to create one. The default direcory is the Documents folder. 
 # New-Item $PROFILE.CurrentUserAllHosts -ItemType File -Force
+New-Item $PROFILE.CurrentUserAllHosts -ItemType File -Force
 
 # Function to access tool.exe from anywhere on the system 
 function iwtool.exe { C:\'Program Files\IceWarp\tool.exe' $args }
 
-
 # Services with Powershell
-```powershell
+
 # Function is like an alias for IceWarp Control 
 function iwctrl {Get-Service -name IceWarpControl}
 # Then we can start, stop, or restart by calling the function 
